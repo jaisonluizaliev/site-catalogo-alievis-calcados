467c81f898ba53602357a075d784ab953dce9403
Jaison Dev <jaison.alievi@hotmail.com>
2020-12-28 19:58:17 -0300

    more details

--- components/Carousel/Image/index.js
+++ components/Carousel/Image/index.js
@@ -1,6 +1,11 @@
 
 const Image = (props) => {
-  return <img className="d-block mx-auto carousel-vh carousel-vh-mob carousel-t" src={props.src} alt={props.alt}/>
+  return <img className="d-block mx-auto carousel-vh carousel-vh-mob carousel-t" 
+  src={props.src} 
+  alt={props.alt}
+  width={600}
+  height={600}
+  />
 }
 
 export default Image
