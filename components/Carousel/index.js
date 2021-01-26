d708ea0412752a804ba877dc6c00359ac1c353cc
jaison alievi <jaison.alieve@gmail.com>
2021-01-26 10:33:47 -0300

    added modify

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -34,15 +34,24 @@ const CarouselComp = () => {
         <Carousel.Item interval={2000}>
           <a href="/peeptoe">
             <Image
-              src="/peeptoe-preto-pagina.jpg"
+              src="/sandalia-bloco-home.jpg"
               alt="peeptoe preto"
             />
           </a>
         </Carousel.Item>
         <Carousel.Item interval={2000}>
+          <a href="/peeptoe">
+            <Image
+              src="/peeptoe-preto-pagina.jpg"
+              alt="peeptoe preto"
+            />
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item interval={2000}>
           <a href="/sapatilhas">
             <img
-              className="slide-home" 
+              className="slide-home"
               src="/sapatilha-napa-chocolate.jpg"
               alt="sandalia em bloco"
             />
